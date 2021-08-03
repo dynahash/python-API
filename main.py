144593f2b074a82c2cd03954317e37eb5a588840
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2021-08-03 20:56:13 -0300

    body created

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from checks.token_check import checkToken
+
+app = Flask('WannaEat')
+
+@app.route("/primaryRoute", methods=["GET"])
+def primary_route():
+    token = request.args.get('token')
+    check = checkToken(token)
+
+    if False in check.keys():
+        return check[False]  # if error, return that string
+    dads = {check[True]:
+                {'hello word': '123'}
+            }
+
+    return dads
+
+
+app.run()
